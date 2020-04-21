e24d51eaca34933fcb5c2f01adddca5d9b179ca0
gzs <gzs@163.com>
2020-04-21 17:55:48 +0800

    create oasystem

---
+++ src/test/java/com/woniuxy/oasystem/OastystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.woniuxy.oasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@SpringBootTest
+class OastystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
