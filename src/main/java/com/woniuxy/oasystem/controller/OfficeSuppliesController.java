76dd3731d97371e893063afa4f08e2e01fcfaf40
张钰平 <zyp2402906495@163.com>
2020-04-22 21:18:16 +0800

    办公用品页面展示

--- src/main/java/com/woniuxy/oasystem/controller/OfficeSuppliesController.java
+++ src/main/java/com/woniuxy/oasystem/controller/OfficeSuppliesController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,13 +32,16 @@ public class OfficeSuppliesController {
 	 * @return
 	 * @changeLog 	1. 创建 (2020年4月22日 [张钰平])
 	 */
-	@RequestMapping("/list")
-
+	@GetMapping("/list")
 	public String findAll(Model model) {
 		List<OfficeSupplies> list = officeSuppliesService.findAll();
 		model.addAttribute("list",list);
-		return "a";
+		return "officeSupplies";
 	}
-	
+	@DeleteMapping("/{osId}")
+	public String delete() {
+		System.out.println(1111);
+		return "officeSupplies";
+	}
 
 }
