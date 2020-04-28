6dc169e1b49efc5420b8e6937955a6b39eafc3c9
lwp <1065041974@qq.com>
2020-04-29 00:06:03 +0800

    # WARNING: head commit changed in the meantime
    LWP文件上传

---
+++ src/main/java/com/woniuxy/oasystem/service/AnnouncementFileService.java
@@ -0,0 +1,28 @@
+/**
+ * @author liuwenpeng
+ * @createDate 2020年4月28日
+ */
+package com.woniuxy.oasystem.service;
+
+import java.util.List;
+
+import com.woniuxy.oasystem.entity.AnnouncementFile;
+
+/**
+ * @Description  TODO
+ * @author liuwenpeng
+ * @changeLog 	1. 创建 (2020年4月28日 下午9:08:42 [liuwenpeng])
+ * 				2.
+ */
+public interface AnnouncementFileService {
+	public void addAnnouncementFile(AnnouncementFile anf);
+
+	/**
+	 * TODO
+	 * @param anId
+	 * @return
+	 * @changeLog 	1. 创建 (2020年4月28日 下午10:48:27 [liuwenpeng])
+	 *              2.查询公告Id下的所有文件
+	 */
+	public List<AnnouncementFile> ShowFiles(Integer anId);
+}
