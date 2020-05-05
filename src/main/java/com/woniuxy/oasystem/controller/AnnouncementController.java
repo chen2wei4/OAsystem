4da3883cf0aa93c16f24cb1162d43d239f15bd6c
a <a@163.com>
2020-05-05 22:56:57 +0800

    报告

--- src/main/java/com/woniuxy/oasystem/controller/AnnouncementController.java
+++ src/main/java/com/woniuxy/oasystem/controller/AnnouncementController.java
@@ -67,7 +67,6 @@ public class AnnouncementController {
 			e.printStackTrace();
 			//发生异常打到错误页
 			return "lyear_pages_error";
-					
 		}
 	}
 
--- src/main/java/com/woniuxy/oasystem/controller/ReportsController.java
+++ src/main/java/com/woniuxy/oasystem/controller/ReportsController.java
@@ -28,6 +28,9 @@ public class ReportsController {
 	// 根据report_from分页查询
 	@GetMapping(value = "/reportByReportFrom")
 	public String reportByReportFrom(HttpServletRequest req, Model model, Reports reports, Integer pageIndex) {
+		System.out.println(reports);
+		System.out.println(reports.getTypeId());
+		System.out.println(reports.getReportName());
 		Emp emp = (Emp) req.getSession().getAttribute("emp");
 		reports.setReportFrom(emp);
 		reports.setReportFromId(emp.getEmpId());
@@ -49,79 +52,112 @@ public class ReportsController {
 		if (pageIndex == null) {
 			pageIndex = 1;
 		}
-		int pagesize = 10;
-		PageBean<Reports> allpb = reportsService.selectAllReportsByConditionPage(reports, pageIndex, pagesize);
-		model.addAttribute("allpb", allpb);
-		return "reoprt_alllist.html";
+		try {
+			int pagesize = 10;
+			PageBean<Reports> allpb = reportsService.selectAllReportsByConditionPage(reports, pageIndex, pagesize);
+			List<ReportType> allReportType = reportTypeService.selectAllReportType();
+			model.addAttribute("reportType", allReportType);
+			model.addAttribute("reports", reports);
+			model.addAttribute("allpb", allpb);
+			return "reoprt_alllist.html";
+		} catch (Exception e) {
+			return "lyear_pages_error";
+		}
+
 	}
 
 	// 软删除
 	@GetMapping(value = "/deleteReport")
 	public String deleteReport(HttpServletRequest req, Model model, Reports reports, Integer pageIndex, int reportId) {
-		Emp emp = (Emp) req.getSession().getAttribute("emp");
-		reports.setReportFrom(emp);
-		reports.setReportFromId(emp.getEmpId());
-		reportsService.deleteReportsByReportId(reportId);
-		if (pageIndex == null) {
-			pageIndex = 1;
+		try {
+			Emp emp = (Emp) req.getSession().getAttribute("emp");
+			reports.setReportFrom(emp);
+			reports.setReportFromId(emp.getEmpId());
+			reportsService.deleteReportsByReportId(reportId);
+			if (pageIndex == null) {
+				pageIndex = 1;
+			}
+			int pagesize = 10;
+			PageBean<Reports> pb = reportsService.selectReportsByReportFromAndConditionPage(reports, pageIndex,
+					pagesize);
+			model.addAttribute("pb", pb);
+			return "reoprt_list.html";
+		} catch (Exception e) {
+			return "lyear_pages_error";
 		}
-		int pagesize = 10;
-		PageBean<Reports> pb = reportsService.selectReportsByReportFromAndConditionPage(reports, pageIndex, pagesize);
-		model.addAttribute("pb", pb);
-		return "reoprt_list.html";
 	}
 
 	// 修改报表页面跳转
 	@GetMapping(value = "/updateReportPageJumps")
 	public String updateReportPageJumps(Model model, Integer reportId) {
-		List<ReportType> allReportType = reportTypeService.selectAllReportType();
-		model.addAttribute("reportType", allReportType);
-		Reports updateReport = reportsService.selectByReportId(reportId);
-		model.addAttribute("updateReport", updateReport);
-		return "reoprt_update.html";
+		try {
+			List<ReportType> allReportType = reportTypeService.selectAllReportType();
+			model.addAttribute("reportType", allReportType);
+			Reports updateReport = reportsService.selectByReportId(reportId);
+			model.addAttribute("updateReport", updateReport);
+			return "reoprt_update.html";
+		} catch (Exception e) {
+			return "lyear_pages_error";
+		}
 	}
 
 	// 修改报表
 	@PostMapping(value = "/updateReport")
 	public String updateReport(HttpServletRequest req, @ModelAttribute Reports reports, Model model) {
-		Date date = new Date();
-		Emp emp = (Emp) req.getSession().getAttribute("emp");
-		reports.setReportFromId(emp.getEmpId());
-		reports.setReportDate(date);
-		reportsService.updateReportsByReportId(reports);
-		// 通过时间查到刚修改的报表跳转到报表详情页
-		Reports reportDetail = reportsService.selectByReportDate(date);
-		model.addAttribute("reportDetail", reportDetail);
-		return "report_detail";
+		try {
+			Date date = new Date();
+			Emp emp = (Emp) req.getSession().getAttribute("emp");
+			reports.setReportFromId(emp.getEmpId());
+			reports.setReportDate(date);
+			reportsService.updateReportsByReportId(reports);
+			// 通过时间查到刚修改的报表跳转到报表详情页
+			Reports reportDetail = reportsService.selectByReportDate(date);
+			model.addAttribute("reportDetail", reportDetail);
+			return "report_detail";
+		} catch (Exception e) {
+			return "lyear_pages_error";
+		}
 	}
 
 	// 新增报表页面跳转
 	@GetMapping(value = "/addReportPageJumps")
 	public String addReportPageJumps(Model model) {
-		List<ReportType> allReportType = reportTypeService.selectAllReportType();
-		model.addAttribute("reportType", allReportType);
-		return "reoprt_add.html";
+		try {
+			List<ReportType> allReportType = reportTypeService.selectAllReportType();
+			model.addAttribute("reportType", allReportType);
+			return "reoprt_add.html";
+		} catch (Exception e) {
+			return "lyear_pages_error";
+		}
 	}
 
 	// 新增报表
 	@PostMapping(value = "/addReport")
 	public String addReport(HttpServletRequest req, @ModelAttribute Reports reports, Model model) {
-		Date date = new Date();
-		Emp emp = (Emp) req.getSession().getAttribute("emp");
-		reports.setReportFromId(emp.getEmpId());
-		reports.setReportDate(date);
-		reportsService.insertReports(reports);
-		// 通过时间查到刚添加的报表跳转到报表详情页
-		Reports reportDetail = reportsService.selectByReportDate(date);
-		model.addAttribute("reportDetail", reportDetail);
-		return "report_detail";
+		try {
+			Date date = new Date();
+			Emp emp = (Emp) req.getSession().getAttribute("emp");
+			reports.setReportFromId(emp.getEmpId());
+			reports.setReportDate(date);
+			reportsService.insertReports(reports);
+			// 通过时间查到刚添加的报表跳转到报表详情页
+			Reports reportDetail = reportsService.selectByReportDate(date);
+			model.addAttribute("reportDetail", reportDetail);
+			return "report_detail";
+		} catch (Exception e) {
+			return "lyear_pages_error";
+		}
 	}
 
 	// 通过Id查详细报表
 	@GetMapping("/reportDetail")
 	public String reportDetail(Integer reportId, Model model) {
-		Reports reportDetail = reportsService.selectByReportId(reportId);
-		model.addAttribute("reportDetail", reportDetail);
-		return "report_detail";
+		try {
+			Reports reportDetail = reportsService.selectByReportId(reportId);
+			model.addAttribute("reportDetail", reportDetail);
+			return "report_detail";
+		} catch (Exception e) {
+			return "lyear_pages_error";
+		}
 	}
 }
